1ef47bc4998077045b5365d3723dd75e0e08cce9
Yuki528 <kiplimomeli44@gmail.com>
2022-09-19 14:21:06 +0300

    advanced

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,11 +7,29 @@
  */
 int _atoi(char *s)
 {
-	int i;
-	char *s;
+	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
 
-	s = "-9885";
+	while (*(s + count) != '\0')
+	{
+		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
+			break;
 
-	i = atoi(s);
-	printf("i = %d \n", i);
+		if (*(s + count) == '-')
+			pn *= -1;
+
+		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+		{
+			if (size > 0)
+				m *= 10;
+			size++;
+		}
+		count++;
+	}
+
+	for (i = count - size; i < count; i++)
+	{
+		oi = oi + ((*(s + i) - 48) * m);
+		m /= 10;
+	}
+	return (oi * pn);
 }
