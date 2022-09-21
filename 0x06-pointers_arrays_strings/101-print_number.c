a0705af5cbf825e05076fade6f8dbe5b3e7c093c
Yuki528 <kiplimomeli44@gmail.com>
2022-09-21 16:13:04 +0300

    advanced

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_number - prints an integer.
+ * @n: input integer.
+ * Return: no return.
+ */
+
+void print_number(int n)
+{
+	if(n < 0){
+		putchar(' - ');
+		n = -n;
+	}
+
+	if(n == 0)
+		putchar('0');
+
+	if(n/10)
+		printnumber(n/10);
+	putchar(n%10 + '0');
+}
