119c64dfe4837f7cc600f15cae9a15ca0af72383
Yuki528 <kiplimomeli44@gmail.com>
2022-09-22 09:21:26 +0300

    printnumber

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,15 +7,29 @@
 
 void print_number(int n)
 {
-	if(n < 0){
-		putchar(' - ');
-		n = -n;
+	unsigned int m, d, count;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		m = n * -1;
+	}
+	else
+	{
+		m = n;
 	}
 
-	if(n == 0)
-		putchar('0');
+	d = m;
+	count = 1;
 
-	if(n/10)
-		printnumber(n/10);
-	putchar(n%10 + '0');
+	while (d > 9)
+	{
+		d /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
+	}
 }
