97fe132cd1dbf75e4108aa10f0a1d01fa97ef8ed
Yuki528 <kiplimomeli44@gmail.com>
2022-09-26 14:02:06 +0300

    pointers

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,22 +1,27 @@
 #include "main.h"
 #include <stdlib.h>
-/**
- *
- * function that searches a string for any of a set of bytes
- *
- * function that searches a string for any of a set of bytes
- *
- */
 
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: string to check
+ * @accept: string to check against
+ *
+ * Return: pointer to byte in s that matches or NULL if no match
+ */
 char *_strpbrk(char *s, char *accept)
 {
 	int i;
+
 	while(*s)
 	{
+		
 		for(i =0; accept[i]; i++)
 		{
+			
 			if(*s == accept[i])
+			{
 				return (s);
+			}
 		}
 		s++;
 	}
