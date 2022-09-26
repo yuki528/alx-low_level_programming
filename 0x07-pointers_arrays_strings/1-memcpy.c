ddeb4295adf2cd4ecf7b3427c84faf7a1e4e9320
Yuki528 <kiplimomeli44@gmail.com>
2022-09-26 14:25:30 +0300

    pointers

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,15 +8,15 @@
  *  @dest: destination of copy
  *  @src: memory area to copy
  */
-
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
+
 	unsigned int i;
 
 	for(i =0; i<n; i++)
-	{
 		dest[i] = src[i];
-	}
+	
 
 	return(dest);
+	
 }
