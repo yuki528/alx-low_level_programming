8020b8cba4cafa61cc8d0269ca63bbc4aa55bef1
Yuki528 <kiplimomeli44@gmail.com>
2022-09-26 14:41:50 +0300

    pointers

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,17 +13,13 @@ unsigned int _strspn(char *s, char *accept)
 	
 	for(i=0; s[i]; i++)
 	{
-
 		for(j=0; accept[j]; j++)
 		{
-
-			 if(s[i] == accept[j])
+			if(s[i] == accept[j])
 				 break;
 		}
-
 		if(!accept[j])
 			break;
 	}
-
 	return(i);
 }
