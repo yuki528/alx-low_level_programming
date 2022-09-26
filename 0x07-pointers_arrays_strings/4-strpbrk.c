8c82574ef50e411914abcc608199cf970f9def78
Yuki528 <kiplimomeli44@gmail.com>
2022-09-26 14:54:21 +0300

    pointers

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,20 +8,18 @@
  *
  * Return: pointer to byte in s that matches or NULL if no match
  */
+
 char *_strpbrk(char *s, char *accept)
 {
 	int i;
 
 	while(*s)
 	{
-		
 		for(i =0; accept[i]; i++)
-		{
-			
+		{	
 			if(*s == accept[i])
-			{
 				return (s);
-			}
+			
 		}
 		s++;
 	}
