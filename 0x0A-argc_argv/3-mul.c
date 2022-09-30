bf194d661db5cc93eb9a80c1c89aef5f0603e8a4
Yuki528 <kiplimomeli44@gmail.com>
2022-09-30 12:49:14 +0300

    arguments

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -6,19 +6,15 @@
  * @argv: array that contains the program command line arguments.
  * Return: 0 - success.
  */
-
 int main (int argc, char *argv[])
 {
-	
 	if(argc != 3)
 	{
 		
 		printf("Error");
-
 		return (1);
 	}
 
 	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-
 	return (0);
 }
