aca1dc0c296202785523c76c4c6bd3c3b029e185
Yuki528 <kiplimomeli44@gmail.com>
2022-09-30 13:13:39 +0300

    arguments

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,10 +7,8 @@
  *
  * Return: always 0
  */
-int main(int argc __atrribute__((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	
 	printf("%s\n", argv[0]);
-	
 	return (0);
 }
